2f7ad7196c1790b5ba56b8949be75eafc309a4d6
shivalli0623 <900131shawn@gmail.com>
2022-05-27 01:26:19 +0800

    gem generation
    1.  random texture when instance gem scene  (gem.tscn)
    2. generate 30 gem in shape of 5 x 6  matrix

--- sence/Main.gd
+++ sence/Main.gd
@@ -1,14 +1,23 @@
 extends Node
 
+var gem_tscn = load("res://sence/gem.tscn")
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var tile_space = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for col in range(5):
+		var raw_list = []
+		for raw in range(6):
+			raw_list.append(gem_tscn.instance())
+		tile_space.append(raw_list)
+	
+	for row in range(5):
+		for col in range(6):
+			var gem = tile_space[row][col]
+			gem.position = Vector2(50 + col * 85, 90 * row + 100)
+			add_child(gem)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- sence/gem.gd
+++ sence/gem.gd
@@ -1,11 +1,28 @@
 extends Sprite
 
+# texture pre-load
+var blue_gem = preload("res://myAsset/Texture2D/gems/normalGems/bule.png")
+var dark_gem = preload("res://myAsset/Texture2D/gems/normalGems/dark.png")
+var green_gem = preload("res://myAsset/Texture2D/gems/normalGems/green.png")
+var pink_gem = preload("res://myAsset/Texture2D/gems/normalGems/pink.png")
+var red_gem = preload("res://myAsset/Texture2D/gems/normalGems/red.png")
+var yellow_gem = preload("res://myAsset/Texture2D/gems/normalGems/yellow.png")
+# texture list
+var GEM_TYPE = [blue_gem, dark_gem, green_gem, pink_gem, red_gem, yellow_gem]
+
 var selected = false
 
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if Input.is_action_just_pressed("click"):
-		selected = true
-		
+func send_argument(kwarg: Dictionary):
+	var gem_type = kwarg["gem_type"]
+	
+func _ready():
+	# random texture
+	randomize()
+	# randi(): Generates a pseudo-random 32-bit unsigned integer between 0 and 4294967295 (inclusive)
+	# [ 4294967295 % 6 = 3 ] mean 0, 1, 2, 3 are redundant
+	# the range is 0 ~ (4294967295 - 4)
+	texture = GEM_TYPE[(randi() - 4) % 6]
+
 func _physics_process(delta):
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
@@ -14,3 +31,7 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and not event.pressed:
 			selected = false
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		selected = true
