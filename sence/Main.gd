aa1acecbe81c4e7025370a893c61632073c2a186
shivalli0623 <900131shawn@gmail.com>
2022-06-15 22:38:54 +0800

    modify
    1. when you dragging gem the last position will has a temp gem  with half alpha value
    2.  find a bug. when you dragging too quickly the gem will stick on your mouse cursor

--- sence/Main.gd
+++ sence/Main.gd
@@ -3,8 +3,17 @@ extends Node
 var gem_tscn = load("res://sence/gem.tscn")
 
 # Declare member variables here. Examples:
-var tile_space = []
+var gem_tilemap = []
 var tile_size = 85
+var last_selected_gem = {
+					"position": [0, 0], 
+					"selected": false,
+					"type": 0,
+					"current_pos": Vector2(0, 0)}
+	
+func get_gem(row: int, col: int):
+	return gem_tilemap[row][col]
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,14 +23,14 @@ func _ready():
 #		for y in range(5):
 #			$TileMap.set_cell(x, y, (randi() - 4) % 6)
 	for col in range(5):
-		var raw_list = []
-		for raw in range(6):
-			raw_list.append(gem_tscn.instance())
-		tile_space.append(raw_list)
+		var row_list = []
+		for row in range(6):
+			row_list.append(gem_tscn.instance())
+		gem_tilemap.append(row_list)
 
 	for row in range(5):
 		for col in range(6):
-			var gem = tile_space[row][col]
+			var gem = gem_tilemap[row][col]
 			gem.position = gem.position.snapped(Vector2.ONE * tile_size)
 			gem.position += Vector2.ONE * (tile_size/2)
 			gem.position += (Vector2.RIGHT * col + Vector2.DOWN * (row)) * tile_size + $TileMap.position
@@ -31,5 +40,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	for row in range(5):
+		for col in range(6):
+			var gem = gem_tilemap[row][col]
+			if gem.selected:
+				last_selected_gem = {
+					"position": [row, col], 
+					"selected": gem.selected,
+					"type": gem.type,
+					"current_pos": gem.current_position}
+#				print(last_selected_gem)
+				$TileMap.set_cell(col, row, gem.type)
+			else:
+				# TODO
+				# trying not to run for all the loop when reset gem
+				$TileMap.set_cell(col, row, -1)		
 	pass
--- sence/gem.gd
+++ sence/gem.gd
@@ -2,33 +2,52 @@ extends Area2D
 
 # texture pre-load
 var blue_gem = preload("res://myAsset/Texture2D/gems/normalGems/bule.png")
-var dark_gem = preload("res://myAsset/Texture2D/gems/normalGems/dark.png")
-var green_gem = preload("res://myAsset/Texture2D/gems/normalGems/green.png")
-var pink_gem = preload("res://myAsset/Texture2D/gems/normalGems/pink.png")
 var red_gem = preload("res://myAsset/Texture2D/gems/normalGems/red.png")
+var green_gem = preload("res://myAsset/Texture2D/gems/normalGems/green.png")
+var dark_gem = preload("res://myAsset/Texture2D/gems/normalGems/dark.png")
 var yellow_gem = preload("res://myAsset/Texture2D/gems/normalGems/yellow.png")
+var pink_gem = preload("res://myAsset/Texture2D/gems/normalGems/pink.png")
 # texture list
-var GEM_TYPE = [blue_gem, dark_gem, green_gem, pink_gem, red_gem, yellow_gem]
+var GEM_TYPE_L = [blue_gem, red_gem, green_gem, yellow_gem, dark_gem, pink_gem]
 
+enum{BLUE, RED, GREEN, YELLOW, DARK, PINK}
 var selected = false
-var fixed_position = null
+var current_position = null
+var type = null
+var gem_drag_speed = 45
 
 
 func send_argument(kwarg: Dictionary):
 	var gem_type = kwarg["gem_type"]
+
 	
 func _ready():
 	# random texture
 	randomize()
 	# randi(): Generates a pseudo-random 32-bit unsigned integer between 0 and 4294967295 (inclusive)
-	# [ 4294967295 % 6 = 3 ] mean 0, 1, 2, 3 are redundant
+	# [ 4294967295 % 6 = 3 ] mean there are 4 redundant number that is  0, 1, 2, 3
 	# the range is 0 ~ (4294967295 - 4)
-	$Sprite.texture = GEM_TYPE[(randi() - 4) % 6]
-	fixed_position = position
+	var type_tmp = GEM_TYPE_L[(randi() - 4) % 6]
+	$Sprite.texture = type_tmp
+	
+	if type_tmp == blue_gem:
+		type = BLUE
+	elif type_tmp == red_gem:
+		type = RED
+	elif type_tmp == green_gem:
+		type = GREEN
+	elif type_tmp == yellow_gem:
+		type = YELLOW
+	elif type_tmp == dark_gem:
+		type = DARK
+	else:
+		type = PINK
+		
+	current_position = position
 
 func _physics_process(delta):
 	if selected:
-		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+		global_position = lerp(global_position, get_global_mouse_position(), gem_drag_speed * delta)
 
 func position_exchange():
 	pass
@@ -39,4 +58,7 @@ func _on_Gem_input_event(viewport, event, shape_idx):
 		if event.is_pressed():
 			selected = true
 		else:
+			# FIXME 
+			# when mouse input release out of the region of collisionshape
+			# gem instance stick on mouse cursor.
 			selected = false
